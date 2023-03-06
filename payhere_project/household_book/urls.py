0638a222043b64eb38b3f7a8ddd0f3f0f482814d
MoonDD99 <mjh991016@naver.com>
2023-03-06 21:33:57 +0900

    [feat] 가계부 내역 리스트, 생성, 삭제, 상세보기, 수정, 복제 기능 추가

--- payhere_project/household_book/urls.py
+++ payhere_project/household_book/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
 
 from household_book.view.book_views import BookView
+from household_book.view.book_record_views import BookRecordView, BookRecordDetailView
 
 urlpatterns = [
-    path('',BookView.as_view())
+    path('',BookView.as_view()),
+    path('<int:book_id>',BookView.as_view()),
+    path('<int:book_id>/records',BookRecordView.as_view()),
+    path('<int:book_id>/records/<int:record_id>',BookRecordDetailView.as_view())
 ]
