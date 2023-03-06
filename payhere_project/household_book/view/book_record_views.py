6e8ca85dcb8f181195615bd3c0c35608f315876a
MoonDD99 <mjh991016@naver.com>
2023-03-06 19:15:18 +0900

    [feat] 가계부 리스트, 생성 기능 추가

---
+++ payhere_project/household_book/view/book_record_views.py
@@ -0,0 +1,7 @@
+#household_book_recod 리스트 GET /household-books/{house-books.id}/records
+#household_book_recod 생성 POST /household-books/{house-books.id}/records
+
+#household_book_recod 상세보기 GET /household-books/{house-books.id}/records/{record.id}
+#household_book_recod 수정 (amount, description) POST /household-books/{house-books.id}/records/{record.id}
+#household_book_recod 삭제 DELETE /household-books/{house-books.id}/records/{record.id}
+#household_book_recod 복제 POST /household-books/{house-books.id}/records/{record.id}
